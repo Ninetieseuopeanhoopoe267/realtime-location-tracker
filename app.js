09d1b293bd8af296e7aae2af36a04e7949d49394
RY18coder <148080068+RY18coder@users.noreply.github.com>
2026-05-19 11:34:41 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
